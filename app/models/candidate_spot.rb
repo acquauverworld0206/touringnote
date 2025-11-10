1dd8afcd8e9b8b44a45e2a987598079b8335f60c
acquauverworld0206 <acquauverworld0206@gmail.com>
2025-11-10 21:54:43 +0900

    candidate_spot.rb修正

--- app/models/candidate_spot.rb
+++ app/models/candidate_spot.rb
@@ -1,7 +1,7 @@
 class CandidateSpot < ApplicationRecord
   belongs_to :group
   belongs_to :spot
-  belongs_to :added_by, class_name: 'User', foreign_key: 'added_by_user_id'
+  belongs_to :added_by, class_name: 'User'
 
   has_many :votes, dependent: :destroy
   has_many :voted_users, through: :votes, source: :user
